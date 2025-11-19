f5fef97bdf11b00955a9d69e8d351bb14fa89bde
Bernardus <bernard@getbaraka.com>
2025-11-19 10:49:24 +0100

    Tracking for production only.

--- app.vue
+++ app.vue
@@ -1,12 +1,22 @@
-<script setup>
-useHead({
-  script: [
-    {
+<script setup lang="ts">
+import type { ResolvableArray, ResolvableScript } from "@unhead/vue/types";
+
+function determineHeadScripts(): ResolvableArray<string | ResolvableScript> {
+  const headScripts = [];
+
+  if (process.env.NODE_ENV === "production") {
+    headScripts.push({
       defer: true,
       src: "https://overarch.bernardus.dev/overarch",
       "data-website-id": "75012deb-308d-452d-9564-043975e7c9a9",
-    },
-  ],
+    });
+  }
+
+  return headScripts;
+}
+
+useHead({
+  script: determineHeadScripts(),
 });
 </script>
 <template>
